7c2f14301e9872293d854a1dd5677465057d6a52
Raman <vincapto@gmail.com>
2022-12-05 01:12:22 +0100

    feat: add zip solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { dirname, join } from 'path';
+import * as zlib from 'zlib';
+import { createWriteStream, createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const name = 'fileToCompress.txt';
+const zippedFile = 'archive.gz';
+const filePath = join(__dirname, 'files', name);
+const zippedFilePath = join(__dirname, 'files', zippedFile);
+
+const zip = zlib.createGzip();
+
 const compress = async () => {
-    // Write your code here 
+  const writeStream = createWriteStream(zippedFilePath);
+  const readStream = createReadStream(filePath);
+  readStream.pipe(zip).pipe(writeStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { dirname, join } from 'path';
+import * as zlib from 'zlib';
+import { createWriteStream, createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const name = 'archive.gz';
+const zippedFile = 'decompressedFile.txt';
+const filePath = join(__dirname, 'files', name);
+const zippedFilePath = join(__dirname, 'files', zippedFile);
+
+const zip = zlib.createUnzip();
+const readStream = createReadStream(filePath);
+const writeStream = createWriteStream(zippedFilePath);
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    readStream.pipe(zip).pipe(writeStream);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await decompress();
