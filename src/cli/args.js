2d97df9dd559962c4dba9d39188310f1ea58f4ad
Raman <vincapto@gmail.com>
2022-12-05 01:03:38 +0100

    feat: add cli solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,30 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import * as readline from 'readline';
+
+const line = readline.createInterface({
+	input: process.stdin,
+});
+
+function writeLine(data) {
+	const paramList = data.split('--');
+	const result = paramList.reduce((acc, next) => {
+		if (next.length === 0) return acc;
+		const [key, val] = next.trim().split(' ');
+		return [...acc, `${key} is ${val}`];
+	}, []);
+	console.log(result.join(', '));
+}
+
+console.log('Enter args \n');
+
 const parseArgs = () => {
-    // Write your code here 
+	line.on('line', (data) => {
+		if (data !== 'exit') writeLine(data);
+		else goodbye();
+		console.log('enter next line');
+	});
 };
 
 parseArgs();
